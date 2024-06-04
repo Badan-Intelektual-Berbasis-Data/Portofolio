e090552d76a9540810f3f7931c0ed6eeadfb84f3
nindiappc <nindi.nindia44@gmail.com>
2024-06-04 14:48:22 +0700

    first project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <div>
+      <div>
+        <h1 className='text-6xl'>Hello World</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
